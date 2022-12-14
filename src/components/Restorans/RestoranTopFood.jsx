43ed166a9f166ea3621feb8d82207e8b1692a32d
Sinisa198 <sinisaljubanovic76@gmail.com>
2022-12-14 16:25:52 +0100

    Create box, add text and location

--- src/components/Restorans/RestoranTopFood.jsx
+++ src/components/Restorans/RestoranTopFood.jsx
@@ -3,7 +3,17 @@ import logoRestoran from '../../images/logo-restoran.png';
 import miniLogo from '../../images/miniLogo.png';
 import { MdOutlineArrowForwardIos } from 'react-icons/md';
 import { MdFavoriteBorder } from 'react-icons/md';
-import topFood from '../../images/restorantopfood.png';
+import firstimageRestoran from '../../images/firstimageRestoran.png';
+import map from '../../images/map.png';
+
+const listpictures = [
+  {
+    image: '',
+    name: 'Lorem Ipsum',
+    description: 'RESTORANI * Lorem ipsum',
+    rating: '4.2',
+  },
+];
 const RestoranTopFood = () => {
   return (
     <div className='restoran-top-food'>
@@ -28,7 +38,7 @@ const RestoranTopFood = () => {
       </div>
       <div className='box-together'>
         <div className='box'>
-          <img src={topFood} alt='' className='background-for-box' />
+          <img src={firstimageRestoran} alt='' className='background-for-box' />
           <img src={logoRestoran} alt='' className='logo-for-restoran' />
         </div>
         <div className='second-box'>
@@ -57,6 +67,36 @@ const RestoranTopFood = () => {
           </div>
           <div className='nav-galerija'>
             <p>Galerija</p>
+          </div>
+        </div>
+        <div className='text-and-location'>
+          <p className='text-restoran'>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur
+            <br /> adipiscing elit. Nullam mi elit, commodo nec
+            <br /> ante id, ornare efficitur dui. Nulla in quam sed
+            <br />
+            ex aliquam feugiat. In varius risus est, sed
+            <br /> placerat tortor mollis a. Donec nec aliquet ante. <br />
+            Duis dictum, enim vel feugiat ultrices, sapien <br />
+            massa lobortis erat, non mollis libero nisl quis
+            <br /> quam. Nunc tristique eget metus a dictum. <br />
+            Donec tincidunt tincidunt tellus vel sodales.
+            <br /> Orci varius natoque penatibus et magnis dis
+            <br /> parturient montes, nascetur ridiculus mus. Sed
+            <br /> sapien urna, suscipit nec viverra at,
+            <br /> congue at quam. Donec sodales ante a metus consequat,
+            <br />
+            vitae placerat mi maximus. Pellentesque <br />
+            elementum nibh vitae lectus facilisis, in ornare
+            <br /> turpis suscipit.
+          </p>
+          <div className='location'>
+            <p>Restoran Top Food 021</p>
+            <p>Svetozala Miletica 26, 21000 Novi Sad</p>
+            <p>0644226471</p>
+            <img src={map} className='image-map' alt='' />{' '}
+            {/*Stavljena slika samo zbog pregleda*/}
           </div>
         </div>
       </div>
