b2cd768cff62bbe0fe0e0b684ab385b838bedd26
Sinisa198 <sinisaljubanovic76@gmail.com>
2023-01-27 11:32:05 +0100

    Create buttons with background red, and menu for today

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomePageHeader from '../../components/HomePageHeader/HomePageHeader';
 import Header from '../../components/Header/Header';
 
 const ListRestorans = () => {
+  const [activeButton, setActiveButton] = useState('');
+
+  function handleClick(id) {
+    setActiveButton(id);
+  }
   return (
-    <div className='home-page-header'>
+    <div className='home-page-wrapper'>
       <Header />
       <HomePageHeader />
+      <div className='div-main-homepage'>
+        <p className='text-for-meny'>Dnevni meni 1/27/2023</p>
+        <div className='day-menu'>
+          <button
+            id='button1'
+            style={{
+              backgroundColor: activeButton === 'button1' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button1')}
+          >
+            Ponedeljak
+          </button>
+          <button
+            id='button2'
+            style={{
+              backgroundColor: activeButton === 'button2' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button2')}
+          >
+            Utorak
+          </button>
+          <button
+            id='button3'
+            style={{
+              backgroundColor: activeButton === 'button3' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button3')}
+          >
+            Sreda
+          </button>
+          <button
+            id='button4'
+            style={{
+              backgroundColor: activeButton === 'button4' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button4')}
+          >
+            Cetvrtak
+          </button>
+          <button
+            id='button5'
+            style={{
+              backgroundColor: activeButton === 'button5' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button5')}
+          >
+            Petak
+          </button>
+          <button
+            id='button6'
+            style={{
+              backgroundColor: activeButton === 'button6' ? 'red' : 'white',
+            }}
+            className='button-for-days'
+            onClick={() => handleClick('button6')}
+          >
+            Subota
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
