4ac752b30a233ef13aa7f65f7ba0ec57f22466f0
Sinisa198 <sinisaljubanovic76@gmail.com>
2023-02-03 19:58:31 +0100

    Create header for about us, and style

---
+++ src/components/HeaderAboutUs/HeaderAboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const HeaderAboutUs = () => {
+  return (
+    <div className='about-us-header-wrapper'>
+      <Header />
+      <h3 className='header-title'>dunda</h3>
+    </div>
+  );
+};
+
+export default HeaderAboutUs;
---
+++ src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeaderAboutUs from '../../components/HeaderAboutUs/HeaderAboutUs';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <HeaderAboutUs />
+    </div>
+  );
+};
+
+export default AboutUs;
