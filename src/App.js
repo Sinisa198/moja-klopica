7696418b57979d7b4e7cb545cbd9ab4e7fee7e75
Sinisa198 <sinisaljubanovic76@gmail.com>
2023-02-06 16:06:28 +0100

    Create pages, about, faq, privacypolicy and termsofuse for main page AboutUs, and styled all pages

--- src/App.js
+++ src/App.js
@@ -7,24 +7,29 @@ import RestoranTopFood from './pages/Restorans/RestoranTopFood';
 import Menu from './pages/Restorans/Menu/Menu';
 import Impressions from './pages/Restorans/Impressions/Impressions';
 import Gallery from './pages/Restorans/Gallery/Gallery';
-import FrequentlyAskedQuestions from './pages/AdditionalInformation/FrequentlyAskedQuestions';
 import PrivacyNotices from './pages/AdditionalInformation/PrivacyNotices';
-import TermsOfUse from './pages/AdditionalInformation/TermsOfUse';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Faq from './pages/AboutUs/Faq';
+import PrivacyPolicy from './pages/AboutUs/PrivacyPolicy';
+import TermsOfUseAboutus from './pages/AboutUs/TermsOfUseAboutUs';
 
 const App = () => {
   return (
     <div className='App'>
       <Routes>
+        <Route path='/' element={<HomePage />} />
+
         <Route path='/topfood' element={<RestoranTopFood />} />
         <Route path='/navigation' element={<Navigation />} />
-        <Route path='/' element={<HomePage />} />
         <Route path='/footer' element={<FooterForRestoran />} />
         <Route path='/menu' element={<Menu />} />
         <Route path='/impressions' element={<Impressions />} />
         <Route path='/gallery' element={<Gallery />} />
-        <Route path='/faq' element={<FrequentlyAskedQuestions />} />
         <Route path='/privacynotices' element={<PrivacyNotices />} />
-        <Route path='/termsofuse' element={<TermsOfUse />} />
+        <Route path='/aboutus' element={<AboutUs />} />
+        <Route path='/aboutus/faq' element={<Faq />} />
+        <Route path='/aboutus/privacypolicy' element={<PrivacyPolicy />} />
+        <Route path='/aboutus/termsofuse' element={<TermsOfUseAboutus />} />
       </Routes>
     </div>
   );
---
+++ src/data/days.js
@@ -0,0 +1,28 @@
+export const days = [
+  {
+    id: '1',
+    name: 'Ponedeljak',
+  },
+  {
+    id: '2',
+    name: 'Utorak',
+  },
+  {
+    id: '3',
+    name: 'Sreda',
+  },
+  {
+    id: '4',
+    name: 'Cetvrtak',
+  },
+  {
+    id: '5',
+    name: 'Petak',
+  },
+  {
+    id: '6',
+    name: 'Subota',
+  },
+];
+
+export default days;
